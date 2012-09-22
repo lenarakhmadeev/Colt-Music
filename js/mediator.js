1f8285e7154c866e40467af832a413da7788824c
Lenar Akhmadeev <lenarakhmadeev@gmail.com>
2012-09-23 02:45:55 +0600

    second

--- js/mediator.js
+++
@@ -1,9 +0,0 @@
-
-define(['backbone'], function(Backbone) {
-  var mediator;
-  return mediator = {
-    subscribe: Backbone.Events.on,
-    unsubscribe: Backbone.Events.off,
-    publish: Backbone.Events.trigger
-  };
-});
