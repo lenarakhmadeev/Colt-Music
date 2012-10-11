e50464077301bf3cfab3cab0880516a8f6d105df
lenarakhmadeev <lenarakhmadeev@gmail.com>
2012-10-12 01:25:35 +0600

    scroll

--- scripts/config.js
+++ scripts/config.js
@@ -44,7 +44,7 @@ require.config({
   },
   config: {
     'services/proxy/lastFmProxy': {
-      lastFm_key: 'b25b959554ed76058ac220b7b2e0a026',
+      lastFm_key: '6e827e122dacfa2346e88ef5a964b196',
       lastFm_url: 'http://ws.audioscrobbler.com/2.0/'
     },
     'services/player': {
