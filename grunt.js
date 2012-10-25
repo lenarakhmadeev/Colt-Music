717bc91b07226ee1d1b214b1fda0f00c3a5133d5
lenarakhmadeev <lenarakhmadeev@gmail.com>
2012-10-25 16:13:27 +0600

    nothing

--- grunt.js
+++ grunt.js
@@ -1,69 +1,69 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Load modules
-  grunt.loadNpmTasks('grunt-contrib');
-  grunt.loadNpmTasks('grunt-coffee');
-  grunt.loadNpmTasks('grunt-compass');
+	// Load modules
+	grunt.loadNpmTasks('grunt-contrib');
+	grunt.loadNpmTasks('grunt-coffee');
+	grunt.loadNpmTasks('grunt-compass');
 
 
-  // Project configuration.
-  grunt.initConfig({
+	// Project configuration.
+	grunt.initConfig({
 
-    clean: {
-      build: 'build'
-    },
+		clean: {
+			build: 'build'
+		},
 
-    copy: {
-      build: {
-        files: {
-          'build/': 'app/**/*.!(coffee|sass|scss)'
-        }
-      }
-    },
+		copy: {
+			build: {
+				files: {
+					'build/': 'app/**/*.!(coffee|sass|scss)'
+				}
+			}
+		},
 
-    coffee: {
-      build: {
-        options: {
-          base_path: 'app/scripts',
-          preserve_dirs: true
-        },
+		coffee: {
+			build: {
+				options: {
+					base_path: 'app/scripts',
+					preserve_dirs: true
+				},
 
-        src: 'app/scripts/**/*.coffee',
-        dest: 'build/scripts'
-      }
-    },
+				src: 'app/scripts/**/*.coffee',
+				dest: 'build/scripts'
+			}
+		},
 
-    server: {
-      port: 8000,
-      base: 'build'
-    },
+		server: {
+			port: 8000,
+			base: 'build'
+		},
 
-    compass: {
-      build: {
-        src: 'app/styles',
-          dest: 'build/styles',
-          linecomments: true,
-          forcecompile: true,
-          images: 'build/images',
-          relativeassets: true
-      }
-    },
+		compass: {
+			build: {
+				src: 'app/styles',
+				dest: 'build/styles',
+				linecomments: true,
+				forcecompile: true,
+				images: 'build/images',
+				relativeassets: true
+			}
+		},
 
-    concat: {
-      css: {
-        src: 'build/styles/**/*.css',
-        dest: 'build/styles/all.css'
-      }
-    },
+		concat: {
+			css: {
+				src: 'build/styles/**/*.css',
+				dest: 'build/styles/all.css'
+			}
+		},
 
-    watch: {
-      files: 'app/**',
-      tasks: 'build'
-    }
+		watch: {
+			files: 'app/**',
+			tasks: 'build'
+		}
 
-  });
+	});
 
-  grunt.registerTask('build', 'clean:build coffee:build copy:build compass:build concat:css');
-  grunt.registerTask('run', 'build server watch');
+	grunt.registerTask('build', 'clean:build coffee:build copy:build compass:build concat:css');
+	grunt.registerTask('run', 'build server watch');
 
 };
